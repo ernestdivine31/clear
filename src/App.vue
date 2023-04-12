9e5931bf15edd98874aab8109f3c311445ea1054
ernestdivine31 <ernestdivine31@gmail.com>
2023-04-12 17:43:46 +0100

    smoke-work

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Smoke from './components/Smoke.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+      <Smoke msg="You did it!" />
+    </div>
+
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+  
+}
+</style>
